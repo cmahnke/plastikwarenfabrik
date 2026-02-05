18c682f09591c92f5fc0416411d5660e5407cac7
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-02-05 02:44:27 +0100

    Fix JXL version

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -2,8 +2,8 @@
 
 CTX_PATH="$(dirname $(realpath $0))"
 
+$CTX_PATH/../themes/projektemacher-base/scripts/github/jxl-0.11.1.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
-$CTX_PATH/../themes/projektemacher-base/scripts/github/jxl-0.9.1.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-inkscape.sh
